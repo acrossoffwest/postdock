2c0b3b1a43759e6bd8be45f6077536c2c71a6f4b
Yura Karpov <acrossoffwest@gmail.com>
2019-09-24 11:41:41 +0200

    init

--- src/pgsql/extensions/bin/extensions/legacy_functions/install.sh
+++ src/pgsql/extensions/bin/extensions/legacy_functions/install.sh
@@ -3,5 +3,14 @@
 set -e
 
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
-   CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+    CREATE FUNCTION IF NOT EXISTS pg_current_xlog_location()\$\$
+    BEGIN
+        RETURN pg_current_wal_lsn();
+    END;
+    \$\$ LANGUAGE plpgsql;
+    CREATE FUNCTION IF NOT EXISTS pg_last_xlog_receive_location()\$\$
+    BEGIN
+        RETURN pg_last_wal_receive_lsn();
+    END;
+    \$\$ LANGUAGE plpgsql;
 EOSQL
